3abed573ea330f542cd32802840bb0ead4d29282
VshalSankr <vishal.kuzhimanamundottu@people10.com>
2024-04-16 12:21:03 +0530

    small fix

--- features/pageobjects/bundles.page.js
+++ features/pageobjects/bundles.page.js
@@ -4,12 +4,23 @@ class Bundles{
         return $('button[data-hook="select-tier-2"]');
     }
 
+    get continueBtn(){
+        return $('button[data-hook="bundles-page_continue"]');
+    }
+
     continueToTravelers=async()=>{
 
         let tierTwoButton = this.tierTwoSelector;
         await tierTwoButton.waitForExist();
         await tierTwoButton.waitForClickable();
         await tierTwoButton.click();
+
+        
+        let continueBtn = this.continueBtn;
+        await continueBtn.waitForExist();
+        await continueBtn.waitForClickable();
+        await continueBtn.click();
+
 
 
     }
